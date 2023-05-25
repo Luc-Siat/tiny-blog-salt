f990d01e6390de3a39117dbfbfc997eb7ddf0575
luc siat <lucsiat@gmail.com>
2023-05-25 10:16:08 +0200

    refacto

--- tiny-blog/src/App.tsx
+++ tiny-blog/src/App.tsx
@@ -3,13 +3,30 @@ import './App.css'
 import { getPostFromApi } from './services/dummyjsonApi';
 import { IPost } from './models/interfaces';
 import { PostSection } from './components/PostSection';
-import { getRandomImagesfromApi } from './services/loremPicsumApi';
 import { Navbar } from './components/Navbar';
+import { json } from 'stream/consumers';
+
+type LoadingState = {
+  status: "LOADING",
+}
+
+type ErrorState = {
+  status: "ERROR",
+  error: { code: string, message: string }
+}
+
+type OkState = {
+  status: "OK",
+  data: { posts: IPost[], sections: string[]}
+}
+
+type State = LoadingState | ErrorState | OkState;
 
 function App() {
   const [posts, setPosts] = useState<IPost[]>([]);
   const [sections, setSections] = useState<string[]>([]);
   const [filter, setFilter] = useState<string>("");
+  const [state, setState] = useState<State>({status: "LOADING"});
 
   const getRandomSections = (posts : IPost[]) => {
     const tags : string[] = [];
@@ -21,20 +38,45 @@ function App() {
         }
         tags.push(tag);
       });
-      setSections(tags);
     }
+    return tags;
   }
 
   const getData = async () => {
     const posts = await getPostFromApi();
-    getRandomSections(posts);
-    setPosts(posts);
+    if (!posts.ok) {
+      setState({error: posts.error, status: "ERROR"})
+    } else {
+      setState({data: {posts: posts.data, sections: getRandomSections(posts.data) }, status: "OK"})
+      // setSections(getRandomSections(posts));
+      // setPosts(posts);
+    }
   }
 
   useEffect(() => {
-    getData();
+l
   }, [])
 
+
+  switch (state.status) {
+    case "LOADING":
+      return (
+        <p>Loading..</p>
+      )
+      break;
+    case "ERROR":
+      return (
+        <p>error</p>
+      )
+
+    case "OK":
+      return (
+        <pre>{JSON.stringify(state.data, null, 2)}</pre>
+      )
+
+    default:
+      break;
+  }
   return (
     <>
       <Navbar />
@@ -45,7 +87,6 @@ function App() {
         <p>select your favorite tag here! <i className="fa-solid fa-arrow-down"></i></p>
         <select className="text-center text-2xl border rounded-full" onChange={(e) => setFilter(e.target.value)} name="filter" id="filter">
           {sections.map(section => <option value={section}>{section}</option> )}
-
         </select>
         {
           sections.filter(section => section == filter || filter == "").map(section => <PostSection posts={posts.filter(post => post.tags.some(tag => tag == section))} name={section} key={section}/>)
